d648362dc2b1c2a4dfcf615d77ee105cb43cf3f1
Paul Soucy <paulsoucy1@gmail.com>
2024-10-22 17:04:02 -0400

    more debugging

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,8 @@ async fn pump_frames(mut tcp_stream: TcpStream, can_socket: &mut CanSocket) -> a
                 match f {
                     Some(Ok(f)) => {
                         // debug!("CAN => TCP [{:x}]", f.id_word());
-                        if f.id_word() == 0x520 || f.id_word() == 0x5a0 {
-                            debug!("CAN => TCP UDS DATA {:02x?}", f.data());
+                        if f.id_word() & 0x520 > 0 || f.id_word() & 0x5a0 > 0 {
+                            debug!("CAN => TCP UDS {:02x?} DATA {:02x?}", f.id_word(), f.data());
                         }
                         
                         if let Err(e) = tcp_writer.send(f).await {
@@ -88,8 +88,8 @@ async fn pump_frames(mut tcp_stream: TcpStream, can_socket: &mut CanSocket) -> a
                 match f {
                     Some(Ok(f)) => {
                         // debug!("TCP => CAN [{:x}]", f.id_word());
-                        if f.id_word() == 0x520 || f.id_word() == 0x5a0 {
-                            debug!("TCP => CAN UDS DATA {:02x?}", f.data());
+                        if f.id_word() & 0x520 > 0 || f.id_word() & 0x5a0 > 0 {
+                            debug!("TCP => CAN UDS {:02x?} DATA {:02x?}", f.id_word(), f.data());
                         }
                         if let Err(e) = can_socket.send(f).await {
                             error!("error sending frame: {}", e);
